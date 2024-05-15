a8ec708877ccf7860c235b8c884765f1a661e352
Cheta008 <destinychukwuma8@gmail.com>
2024-05-14 17:29:54 -0700

    Edited script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
         function read() {
           let name = prompt("What's your name?");
           let book = prompt(
-            "What is your favorite genre of book? Type 'all' if you love all fantasy novels."
+            "What is your favorite genre of book? Type 'all' if you aren't sure"
           );
 
           if (book === "romance") {
-            alert("OMG! " + name + ", I love romance too 🤭");
+            alert("OMG " + name + "!" + ", I love romance too 🤭");
           } else if (book === "all") {
             alert(
-              "WOW! " +
-                name +
-                ", Hope you enjoy reading fantansy novels on Wattpad 😄"
+              "That's awesome " +
+                name + "!" +
+                ", Hope you enjoy reading on Wattpad 😄"
             );
           } else {
             alert(
-              "That's cool " +
-                name +
-                ", Hope you enjoy reading your " +
+              "That's lovely " +
+                name + "!" +
+                ", Hope you enjoy reading " +
                 book +
                 " books on Wattpad"
             );
